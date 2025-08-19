27da7fb767aa40810176e67da4f030817409d6cd
Watson FOURPENCE <watsonxd4@gmail.com>
2025-08-19 11:21:10 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 
+navigator.getBattery().then(function(battery) {
+  function updateBattery() {
+    const level = Math.floor(battery.level * 100);
+    document.getElementById('battery').textContent = `🔋 Battery: ${level}%`;
+  }
+  updateBattery();
+  battery.addEventListener('levelchange', updateBattery);
+});
+
